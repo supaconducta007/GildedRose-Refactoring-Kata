75815ee3aadfa7618b3501f71579b1fdc8c60c52
supaconducta007 <78935412+supaconducta007@users.noreply.github.com>
2021-02-11 13:59:28 -0800

    Update gilded_rose.js
    Improving the code and add "Conjured Mana Cake" to the list.

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -1,5 +1,5 @@
 class Item {
-  constructor(name, sellIn, quality){
+  constructor(name, sellIn, quality) {
     this.name = name;
     this.sellIn = sellIn;
     this.quality = quality;
@@ -7,53 +7,46 @@ class Item {
 }
 
 class Shop {
-  constructor(items=[]){
+  constructor(items = []) {
     this.items = items;
   }
+
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1;
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1;
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
-        }
+      switch (this.items[i].name) {
+        case "Aged Brie":
+          this.items[i].quality++;
+          this.items[i].sellIn--;
+          if (this.items[i].sellIn < 0) this.items[i].quality++;
+          if (this.items[i].quality > 50) this.items[i].quality = 50;
+          break;
+
+        case "Backstage passes to a TAFKAL80ETC concert":
+          this.items[i].quality++;
+          if (this.items[i].sellIn < 11) this.items[i].quality++;
+          if (this.items[i].sellIn < 6) this.items[i].quality++;
+          //---------------------------
+          this.items[i].sellIn--;
+          // note: I believe this is wrong, but this is the logic that is shown in the original code.
+          // and match with all the result of the original code.  we should update quality after updating sellIN.
+          // It depend how you interpret the problem. Please see my gilded_rose2.js
+          if (this.items[i].quality > 50) this.items[i].quality = 50;
+          if (this.items[i].sellIn < 0) this.items[i].quality = 0;
+          break;
+
+        case "Sulfuras, Hand of Ragnaros":
+          break;
+        case "Conjured Mana Cake":
+          this.items[i].quality -= 2;
+          this.items[i].sellIn--;
+          if (this.items[i].sellIn < 0) this.items[i].quality -= 2;
+          if (this.items[i].quality < 0) this.items[i].quality = 0;
+          break;
+        default:
+          this.items[i].quality--;
+          this.items[i].sellIn--;
+          if (this.items[i].sellIn < 0) this.items[i].quality--;
+          if (this.items[i].quality < 0) this.items[i].quality = 0;
       }
     }
 
@@ -63,5 +56,5 @@ class Shop {
 
 module.exports = {
   Item,
-  Shop
-}
+  Shop,
+};
